90053f68355bebb3114c29ff8804ae66a8ad7612
ruijuanwang <1938608827@qq.com>
2020-02-29 18:17:09 +0800

    初始化项目-安装并在main.js中引入ElementUI

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui' // 引入模块
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+Vue.use(ElementUI) // 全局注册element-ui 框架 一旦全局注册 所有的位置都可以使用组件
+// Vue.use实际上 会调用 ElementUI对象里的一个方法 install方法 并且传入当前的Vue对象
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  router, // es6 挂载路由
   render: h => h(App)
 }).$mount('#app')
