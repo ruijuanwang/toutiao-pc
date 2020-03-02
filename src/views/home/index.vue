c034ddf2b8cbe66b83174f2adf9807df04df6767
ruijuanwang <1938608827@qq.com>
2020-03-02 19:42:59 +0800

    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,22 @@
 <template>
-<div>
-    我是主页home
-</div>
+<!-- 主页 -->
+<!-- el-container组件是最外层的大容器 -->
+<el-container>
+    <!-- el-aside侧边栏容器 -->
+    <el-aside>左侧导航</el-aside>
+    <!-- 右边在嵌套一个外层的大容器 -->
+    <el-container>
+        <!-- 再上下布局 -->
+        <el-header>头部</el-header>
+        <!-- 主要区域 -->
+        <el-main>
+            主体
+            <!-- 这里应该放置二级路由容器 -->
+            <router-view>s</router-view>
+        </el-main>
+    </el-container>
+</el-container>
+
 </template>
 
 <script>
