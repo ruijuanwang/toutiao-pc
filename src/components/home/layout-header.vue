17e3f74630ad64cb9e68f8d3c751e0804dba9160
ruijuanwang <1938608827@qq.com>
2020-03-03 18:00:31 +0800

    头部结构及下拉菜单

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,68 @@
+<template>
+<!-- 头部组件 -->
+    <!-- 栅格布局 24份 el-row: 行 el-col： 列-->
+    <!-- 一行分两列 一列占12份 -->
+  <el-row class="layout-header" type="flex" align="middle">
+      <!-- 左侧 -->
+     <el-col :span="12" class="left">
+         <!-- 字体图标 -->
+         <i class="el-icon-s-fold"></i>
+        <span> 更多新闻关注今日头条</span>
+     </el-col>
+     <!-- 右侧 -->
+     <el-col :span="12" class="right">
+         <!-- 再放一个el-row组件 type="flex" 开启flex布局 justify 设置对齐样式 align属性设置垂直对齐方式-->
+        <el-row type="flex" justify="end" align="middle">
+            <!-- 头像图片 -->
+            <img src='../../assets/img/login_bg.jpg'>
+            <!-- 下拉菜单  用el-dropdown 标签组件  trigger属性：触发下拉的行为 click或hover-->
+            <el-dropdown trigger='click' :hide-on-click="false">
+                   <!-- 昵称 -->
+                   <span class="click">胤馨 <i class="el-icon-arrow-down el-icon--right"></i></span>
+                <!-- 下拉内容需要做具名插槽dropdown  el-dropdown-menu是专门做下拉的组件 -->
+                <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>个人信息</el-dropdown-item>
+                <el-dropdown-item>git地址</el-dropdown-item>
+                <el-dropdown-item divided>退出</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+
+        </el-row>
+     </el-col>
+</el-row>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.layout-header{
+    height: 60px;
+    .left{
+    i {
+        font-size: 20px;
+    }
+    span{
+        font-size: 20px;
+    }
+}
+  .right{
+     img{
+        width: 40px;
+        height:40px;
+        border-radius: 50%;
+        margin-right:5px;
+
+    }
+    .click{
+        cursor: pointer;
+    }
+}
+
+}
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,14 +3,17 @@
 <!-- 最外层放置一个大容器 el-container组件-->
 <el-container>
     <!-- el-aside侧边栏容器 -->
-    <el-aside>
+    <el-aside style="width:230px">
         <!-- 左侧导航组件 -->
      <layout-aside></layout-aside>
     </el-aside>
     <!-- 右边在嵌套一个外层的大容器 -->
     <el-container>
         <!-- 再上下布局 -->
-        <el-header>头部</el-header>
+        <el-header>
+            <!-- 头部组件 -->
+            <layout-header></layout-header>
+        </el-header>
         <!-- 主要区域 -->
         <el-main>
             主体
@@ -25,10 +28,12 @@
 <script>
 // 引入左侧导航组件注册并且使用
 import LayoutAside from '@/components/home/layout-aside'
+import LayoutHeader from '@/components/home/layout-header'// 引入头部组件
 
 export default {
   components: {
-    'layout-aside': LayoutAside// 注册左侧导航组件
+    'layout-aside': LayoutAside, // 注册左侧导航组件
+    'layout-header': LayoutHeader// 注册头部组件
   }
 
 }
