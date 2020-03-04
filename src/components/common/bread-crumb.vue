d476aacae2bcb81663cf4001cab9275449bb8413
ruijuanwang <1938608827@qq.com>
2020-03-04 22:45:36 +0800

    面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,24 @@
+<template>
+<!-- 面包屑组件 el-breadcrumb -->
+
+<!-- separator属性 是分隔符 -->
+<el-breadcrumb separator=">">
+    <!-- :to 跳转的地址 和router-link中的to一样 -->
+  <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+  <!-- 第二个是动态的内容 需要采用传值的方式传进来  一种是props传值 一种是slot -->
+  <el-breadcrumb-item>
+    <!-- 定义一个具名插槽 -->
+    <slot name="title"></slot>
+  </el-breadcrumb-item>
+</el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -2,13 +2,15 @@
 
 // Vue.use会调用对象中的方法 install方法 第一个参数是Vue对象
 // 引入组件
-import LayoutAside from './home/layout-aside'
-import LayoutHeader from './home/layout-header'
+import LayoutAside from './home/layout-aside'// 侧边栏组件
+import LayoutHeader from './home/layout-header'// 头部组件
+import BreadCrumb from './common/bread-crumb' // 面包屑组件
 
 export default {
   install: function (Vue) {
     //   注册全局组件  Vue 一旦注册 在任意位置都可以使用
     Vue.component('layout-aside', LayoutAside)// 注册头部组件
-    Vue.component('layout-header', LayoutHeader)// 注册左侧导航
+    Vue.component('layout-header', LayoutHeader)// 注册左侧导航组件
+    Vue.component('bread-crumb', BreadCrumb)// 注册面包屑组件
   }
 }
