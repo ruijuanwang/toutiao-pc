d36bc2bb21fedddf673b05185ffea4663f3d1a0f
ruijuanwang <1938608827@qq.com>
2020-03-06 21:28:53 +0800

    素材列表-页面结构-tab标签

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -8,12 +8,22 @@
     <!-- 面包屑组件 具名插槽 -->
     <template slot="title">素材管理</template>
   </bread-crumb>
+  <!-- tab标签组件 tab-click  -->
+ <!--  v-model="activeName" 绑定的值，就是所激活的页签  选项卡的 name -->
+  <el-tabs v-model="activeName" type="boder-card">
+    <el-tab-pane label="全部" name="all" >全部素材</el-tab-pane>
+    <el-tab-pane label="收藏" name="collect">收藏素材</el-tab-pane>
+  </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all' // 当前激活的页签 默认all全部
+    }
+  }
 }
 </script>
 
