858cc84070901cba552b10c2c88e775c43e130b7
ruijuanwang <1938608827@qq.com>
2020-03-06 22:14:55 +0800

    素材列表-数据加载显示图片

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -10,10 +10,20 @@
   </bread-crumb>
   <!-- tab标签组件 tab-click  -->
  <!--  v-model="activeName" 绑定的值，就是所激活的页签  选项卡的 name -->
-  <el-tabs v-model="activeName" type="boder-card">
-    <el-tab-pane label="全部" name="all" >全部素材</el-tab-pane>
-    <el-tab-pane label="收藏" name="collect">收藏素材</el-tab-pane>
+  <el-tabs @tab-click='changeTab' v-model="activeName" type="boder-card">
+    <el-tab-pane label="全部" name="all" >
+          <!--全部 素材图片 -->
+          <div class="img-list">
+            <!-- 素材图片显示 循环list数据-->
+          <el-card v-for="item in list" :key="item.id" class="img-card">
+            <!-- 放置图片 赋值图片地址 -->
+            <img :src="item.url" alt="">
+          </el-card>
+          </div>
+            </el-tab-pane>
+    <el-tab-pane label="收藏" name="collect">收藏素材图片</el-tab-pane>
   </el-tabs>
+
   </el-card>
 </template>
 
@@ -21,12 +31,51 @@
 export default {
   data () {
     return {
-      activeName: 'all' // 当前激活的页签 默认all全部
+      activeName: 'all', // 当前激活的页签 默认all全部
+      list: [] // 放置素材管理的全部数据 收藏和全部的
     }
+  },
+  methods: {
+    // 获取素材管理数据
+    getMaterial () {
+      this.$axios({
+        url: '/user/images', // 地址
+        params: { // get参数 == query参数
+          collect: false // 请求参数 是否是收藏图片
+        }
+      }).then(request => {
+      // 请求数据成功
+        // console.log(request)
+        //  request.data.results 是个数组 把这个数组给data中的变量
+        this.list = request.data.results
+      })
+    },
+    // 切换tab事件
+    changeTab () {
+
+    }
+  },
+  created () {
+    // 实例化之后 调用接口 获取素材管理数据
+    this.getMaterial()
   }
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.img-list{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content:space-around;
+  .img-card{
+     width: 220px;
+     height: 240px;
+     margin-bottom: 20px;
+      img{
+        width: 100%;
+        height: 100%;
+      }
+}
+}
 
 </style>
