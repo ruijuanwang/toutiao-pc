c05710bda8a537c4b8b8ab22976538da9f3b3f45
ruijuanwang <1938608827@qq.com>
2020-03-08 23:32:07 +0800

    发表文章-页面结构-简单实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -5,7 +5,35 @@
     <bread-crumb slot="header">
      <template slot="title">发布文章</template>
     </bread-crumb>
-
+<!-- 表单容器  label标题的宽度在el-form组件写 label-width 属性-->
+<el-form style="margin-left:50px" label-width="100px">
+<el-form-item label="标题：">
+    <!-- 输入框 -->
+    <el-input style="width:60%" placeholder="请输入标题"></el-input>
+</el-form-item>
+<el-form-item label="内容：">
+    <!-- 多行输入 -->
+     <el-input type="textarea" :rows="5" style="width:60%"></el-input>
+</el-form-item>
+<el-form-item label="封面：">
+    <!-- 单选框组 -->
+    <el-radio-group>
+        <el-radio>单图</el-radio>
+        <el-radio>三图</el-radio>
+        <el-radio>无图</el-radio>
+        <el-radio>自动</el-radio>
+    </el-radio-group>
+</el-form-item>
+<el-form-item label="频道：">
+    <!-- 下拉菜单 用 select选择器 -->
+    <el-select></el-select>
+</el-form-item>
+<el-form-item>
+    <!-- 两个按钮  -->
+    <el-button type="success">发表</el-button>
+    <el-button type="info">存入草稿</el-button>
+</el-form-item>
+</el-form>
 </el-card>
 </template>
 
