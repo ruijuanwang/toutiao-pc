aca0be011be6cb1fbc34c67e9be4debdea05e49e
ruijuanwang <1938608827@qq.com>
2020-03-09 00:57:04 +0800

    发表文章-发布文章检验逻辑实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -6,35 +6,39 @@
      <template slot="title">发布文章</template>
     </bread-crumb>
 <!-- 表单容器  label标题的宽度在el-form组件写 label-width 属性-->
-<el-form style="margin-left:50px" label-width="100px">
-<el-form-item label="标题：">
+<!-- :model绑定要校验的数据对象  :rules 当前数据的校验规则  props 绑定要校验的字段 -->
+<el-form ref="myForm" :model="publishForm" :rules="publishRules" style="margin-left:50px" label-width="100px">
+<el-form-item label="标题：" prop="title">
     <!-- 输入框 -->
-    <el-input style="width:60%" placeholder="请输入标题"></el-input>
+    <el-input v-model="publishForm.title" style="width:60%" placeholder="请输入标题"></el-input>
 </el-form-item>
-<el-form-item label="内容：">
+<el-form-item label="内容：" prop="content">
     <!-- 多行输入 -->
-     <el-input type="textarea" :rows="5" style="width:60%"></el-input>
+     <el-input v-model="publishForm.content" type="textarea" :rows="5" style="width:60%"></el-input>
 </el-form-item>
-<el-form-item label="封面：">
+<el-form-item label="封面：" prop="cover">
     <!-- 单选框组 -->
-    <el-radio-group>
-        <el-radio>单图</el-radio>
-        <el-radio>三图</el-radio>
-        <el-radio>无图</el-radio>
-        <el-radio>自动</el-radio>
+    <!-- 封面单选组 绑定的是 封面cover中的type -->
+    <el-radio-group v-model="publishForm.cover.type">
+        <!-- 给每el-radio加上 :label属性 用来显示type的值 -->
+        <!-- 封面类型 -1:自动，0-无图，1-1张，3-3张 -->
+        <el-radio :label="1">单图</el-radio>
+        <el-radio :label="3">三图</el-radio>
+        <el-radio :label="0">无图</el-radio>
+        <el-radio :label="-1">自动</el-radio>
     </el-radio-group>
 </el-form-item>
-<el-form-item label="频道：">
+<el-form-item label="频道：" prop="channel_id">
     <!-- 下拉菜单 用 select选择器 -->
     <!-- v-model的值为当前被选中的el-option的 value 属性值 -->
-    <el-select placeholder="请选择频道">
+    <el-select placeholder="请选择频道" v-model="publishForm.channel_id">
         <!-- 下拉菜单 用v-for循环生成el-option  label 显示值  value 保存值 -->
         <el-option v-for="item in channels" :label="item.name" :value="item.id" :key="item.id" ></el-option>
     </el-select>
 </el-form-item>
 <el-form-item>
     <!-- 两个按钮  -->
-    <el-button type="success">发表</el-button>
+    <el-button @click="publish" type="success">发表</el-button>
     <el-button type="info">存入草稿</el-button>
 </el-form-item>
 </el-form>
@@ -45,7 +49,27 @@
 export default {
   data () {
     return {
-      channels: [] // 接收频道数据 为什么用数组接收 因为接口取过来的是个数组 里面的每一项是个对象
+      // 发布表单数据 校验的数据对象
+      publishForm: {
+        title: '', // 文章标题
+        content: '', // 文章内容
+        // 文章封面
+        cover: {
+          type: 0, // 封面类型 -1:自动，0-无图，1-1张，3-3张
+          images: [] // 字符串的数组 对用type  如果type为 1 images中应该有1个值-type为 3 images中应该有3个值 0为空
+        },
+        channel_id: null // 频道id
+
+      },
+      // 发布表单校验规则
+      publishRules: {
+        title: [{ required: true, message: '文章标题不能为空', trigger: 'blur' }, { min: 5, max: 30, message: '文章标题应该在5-30之间', trigger: 'blur' }],
+        content: [{ required: true, message: '文章内容不能为空', trigger: 'blur' }],
+        channel_id: [{ required: true, message: '频道内容不能为空', trigger: 'blur' }]
+
+      },
+      // 接收频道数据
+      channels: [] // 为什么用数组接收 因为接口取过来的是个数组 里面的每一项是个对象
     }
   },
   methods: {
@@ -57,6 +81,11 @@ export default {
       }).then((result) => {
         this.channels = result.data.channels
       })
+    },
+    // 发表按钮方法
+    publish () {
+      // 手动校验 通过this.$refs.myForm获取表单组件实例 调用validate方法
+      this.$refs.myForm.validate()
     }
   },
   created () {
