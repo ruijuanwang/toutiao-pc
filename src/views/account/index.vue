d7e4eacf18e96f9da7dc33c5f70dda81d6e7a52b
ruijuanwang <1938608827@qq.com>
2020-03-10 23:39:02 +0800

    账户信息-自动校验

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -6,20 +6,20 @@
      <template slot="title"> 账户信息</template>
       </bread-crumb>
       <!-- 表单容器 -->
-      <el-form label-width="100px">
+      <el-form :model="formData" :rules="rules" label-width="100px">
           <!-- 表单域 -->
-        <el-form-item label='用户名：'>
-            <el-input v-model="formdata.name" style="width:30%"></el-input>
+        <el-form-item prop="name" label='用户名：'>
+            <el-input v-model="formData.name" style="width:30%"></el-input>
         </el-form-item>
         <el-form-item label='简介：'>
-            <el-input  v-model="formdata.intro" style="width:30%" type="textear" :rows="3"></el-input>
+            <el-input  v-model="formData.intro" style="width:30%" type="textear" :rows="3"></el-input>
         </el-form-item>
-        <el-form-item label='邮箱：'>
-            <el-input  v-model="formdata.email" style="width:30%"></el-input>
+        <el-form-item prop="email" label='邮箱：'>
+            <el-input  v-model="formData.email" style="width:30%"></el-input>
         </el-form-item>
         <el-form-item label='手机号：'>
             <!-- 手机号不能被修改 手机号是登录的标识 -->
-            <el-input  v-model="formdata.mobile" disabled style="width:30%"></el-input>
+            <el-input  v-model="formData.mobile" disabled style="width:30%"></el-input>
         </el-form-item>
          <el-form-item>
              <!-- 保存按钮 -->
@@ -28,7 +28,7 @@
       </el-form>
       <!-- 头像 -->
       <div  class="head-upload">
-          <img src="../../assets/img/login_bg.jpg" alt="">
+          <img :src="formData.photo? formData.photo : defaultImg" alt="">
       </div>
   </el-card>
 </template>
@@ -38,13 +38,23 @@ export default {
   data () {
     return {
       // 接收表单数据
-      formdata: {
+      formData: {
         name: '', // 用户名
         intro: '', // 头条号简介
         photo: '', // 用户头像
         email: '', // 邮箱
         mobile: '' // 手机号
-      }
+      },
+      //   表单检验
+      rules: {
+        name: [{ required: true, message: '用户名不能为空', trigger: 'blur' },
+          { min: 1, max: 7, message: '用户名的长度为1-7位', trigger: 'blur' }
+        ],
+        email: [{ required: true, message: '邮箱不能为空', trigger: 'blur' },
+          { pattern: /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/, message: '邮箱格式不正确', trigger: 'blur' }]
+      },
+      //   默认头像
+      defaultImg: require('../../assets/img/login_bg.jpg') // 头像变量
     }
   },
   methods: {
@@ -55,7 +65,7 @@ export default {
         url: '/user/profile' // 地址
       }).then(result => {
         //   成功
-        this.formdata = result.data // 赋值给表单对象数据
+        this.formData = result.data // 赋值给表单对象数据
       })
     }
   },
