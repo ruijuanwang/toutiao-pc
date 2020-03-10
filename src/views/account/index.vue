4c8aee1fb49c0f2bdcb7a52c54d1ac384585a098
ruijuanwang <1938608827@qq.com>
2020-03-10 23:24:25 +0800

    账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -9,17 +9,17 @@
       <el-form label-width="100px">
           <!-- 表单域 -->
         <el-form-item label='用户名：'>
-            <el-input style="width:30%"></el-input>
+            <el-input v-model="formdata.name" style="width:30%"></el-input>
         </el-form-item>
         <el-form-item label='简介：'>
-            <el-input style="width:30%" type="textear" :rows="3"></el-input>
+            <el-input  v-model="formdata.intro" style="width:30%" type="textear" :rows="3"></el-input>
         </el-form-item>
         <el-form-item label='邮箱：'>
-            <el-input style="width:30%"></el-input>
+            <el-input  v-model="formdata.email" style="width:30%"></el-input>
         </el-form-item>
         <el-form-item label='手机号：'>
             <!-- 手机号不能被修改 手机号是登录的标识 -->
-            <el-input disabled style="width:30%"></el-input>
+            <el-input  v-model="formdata.mobile" disabled style="width:30%"></el-input>
         </el-form-item>
          <el-form-item>
              <!-- 保存按钮 -->
@@ -27,7 +27,7 @@
        </el-form-item>
       </el-form>
       <!-- 头像 -->
-      <div class="head-upload">
+      <div  class="head-upload">
           <img src="../../assets/img/login_bg.jpg" alt="">
       </div>
   </el-card>
@@ -35,6 +35,33 @@
 
 <script>
 export default {
+  data () {
+    return {
+      // 接收表单数据
+      formdata: {
+        name: '', // 用户名
+        intro: '', // 头条号简介
+        photo: '', // 用户头像
+        email: '', // 邮箱
+        mobile: '' // 手机号
+      }
+    }
+  },
+  methods: {
+    //   获取用户信息
+    getUserInfo () {
+      // 调用接口
+      this.$axios({
+        url: '/user/profile' // 地址
+      }).then(result => {
+        //   成功
+        this.formdata = result.data // 赋值给表单对象数据
+      })
+    }
+  },
+  created () {
+    this.getUserInfo() // 获取用户信息
+  }
 
 }
 </script>
